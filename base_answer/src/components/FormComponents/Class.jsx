13f8f008769c0ecf980c2d993ae8c4f6f07c1af3
Ed Santos <edsantos2822@gmail.com>
2022-09-19 23:45:07 -0300

    build:(Class.jsx): implementando componente <Class />

---
+++ base_answer/src/components/FormComponents/Class.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+
+function Class({ charClass, onChange }) {
+  return (
+    <label htmlFor="class">
+      <p>Classe</p>
+      <input
+        type="text"
+        id="class"
+        name="class"
+        value={ charClass }
+        onChange={ onChange }
+        placeholder="Classe"
+      />
+    </label>
+  );
+}
+
+Class.propTypes = {
+  charClass: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default Class;
