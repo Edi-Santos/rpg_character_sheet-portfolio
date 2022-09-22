71ac85172e4e1d7c23332898c3bb63ea2df5e68f
Ed Santos <edsantos2822@gmail.com>
2022-09-21 21:10:32 -0300

    chore:(Form.jsx): mudando tipo de dados de estados

--- base_answer/src/components/Form.jsx
+++ base_answer/src/components/Form.jsx
@@ -8,17 +8,17 @@ import Str from "./FormComponents/attributes/Str";
 function Form() {
   const [charInfo, setCharInfo] = useState({
     name: '',
-    level: 1,
+    level: '1',
     charClass: '',
   });
 
   const [attrubutes, setAttributes] = useState({
-    str: 0,
-    rec: 0,
-    dex: 0,
-    luc: 0,
-    wis: 0,
-    int: 0,
+    str: '1',
+    rec: '1',
+    dex: '1',
+    luc: '1',
+    wis: '1',
+    int: '1',
   });
 
   const handleChangeInfo = ({ target }) => {
