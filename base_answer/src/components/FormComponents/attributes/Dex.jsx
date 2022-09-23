e99f4715c9b8dca77546de63aa447b211da7fd2c
Ed Santos <edsantos2822@gmail.com>
2022-09-22 23:52:13 -0300

    build:(Dex.jsx): implemente componente <Dex />

---
+++ base_answer/src/components/FormComponents/attributes/Dex.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+
+function Dex({ dex, onChange }) {
+  return (
+    <label htmlFor="dex">
+      <p>DEX</p>
+      <input
+        type="number"
+        id="dex"
+        name="dex"
+        value={ dex }
+        onChange={ onChange }
+      />
+    </label>
+  );
+}
+
+Dex.propTypes = {
+  dex: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default Dex;
